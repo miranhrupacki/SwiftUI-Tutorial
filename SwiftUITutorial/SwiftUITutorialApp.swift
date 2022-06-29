809a6b679c496fc40a18fe9ef4c6e01384c81483
mhrupacki <miran.hrupacki@barrage.net>
2022-06-29 08:32:54 +0200

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Miran Hrupački on 29.06.2022..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
